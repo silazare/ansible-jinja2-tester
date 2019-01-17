ad95fca5cce48c5650ee158d99b720593af9544d
Joel Baranick <joel.baranick@ensighten.com>
2019-01-17 15:18:07 -0800

    Fix app.json

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 DEBUG = os.environ.get('DEBUG', False)
 HOST = os.environ.get('HOST', 'localhost')
-PORT = int(os.environ.get('PORT', 5000))
+PORT = int(os.environ.get('PORT', 80))
 
 LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 LOGGING_LOCATION = 'ansible-jinja2-tester.log'
