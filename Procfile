1d241479bf1a7e572208cdad50e3732bb33b3b89
Joel Baranick <joel.baranick@ensighten.com>
2019-01-18 10:51:30 -0800

    Fix wsgi setup

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi.py --log-config ./logging_config.ini --log-level DEBUG --log-file -
+web: gunicorn wsgi:app --preload --log-config ./logging_config.ini --log-level DEBUG --log-file -
--- config.py
+++ config.py
@@ -3,11 +3,19 @@ import base64
 import os
 import json
 
+
+def _get_firebase_credentials():
+    creds_base64 = os.environ.get('FIREBASE_CREDENTIALS_BASE64')
+    if creds_base64 is None:
+        return None
+    return json.loads(base64.b64decode(creds_base64).decode('utf-8'))
+
+
 DEBUG = os.environ.get('DEBUG', False)
 HOST = os.environ.get('HOST', 'localhost')
 PORT = int(os.environ.get('PORT', 80))
 FIREBASE_URL = os.environ.get('FIREBASE_URL')
-FIREBASE_CREDENTIALS = json.loads(base64.b64decode(os.environ.get('FIREBASE_CREDENTIALS_BASE64')).decode('utf-8'))
+FIREBASE_CREDENTIALS = _get_firebase_credentials()
 
 LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 LOGGING_LOCATION = 'ansible-jinja2-tester.log'
